e0d3b65f0aeed8612e3709768c565fa45b02a09f
shjang7 <suhy.jang.shj@gmail.com>
2020-08-23 17:42:39 +0900

    Feat - Fix error message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import VideoSearch from './components/videoSearch';
 import VideoCard from './components/videoCard';
 
 function App() {
   return (
     <Router>
+      <Helmet>
+        <title>SearchTube</title>
+      </Helmet>
       <Switch>
         <Route exact path="/" component={VideoSearch} />
         <Route exact path="/video" component={VideoCard} />
